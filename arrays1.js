66fde56aa69399fdd323bb7c241f4574de7e2d36
SALEM .. <salemsnsalmutairi@gmail.com>
2024-10-08 11:31:44 +0300

    salem

---
+++ arrays1.js
@@ -0,0 +1,17 @@
+const groceries = ["milk", "eggs", "veggies", "bread", "meats", "drinks"];
+console.log(groceries[1]);
+console.log(groceries.length);
+console.log(groceries[groceries.length - 1]);
+console.log(groceries.pop());
+groceries.push("fish");
+groceries.push("coffee");
+console.log(groceries);
+
+const firstThreeItems = [groceries.slice(0, 3)];
+console.log(firstThreeItems);
+
+//bonus challange, had to look it up.
+// groceries.splice(2, 1);
+// groceries.unshift("fruits");
+// groceries.splice(0, 2, "ketchup", "chili");
+// console.log(groceries);
